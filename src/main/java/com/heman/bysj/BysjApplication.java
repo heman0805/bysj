1ae76280db7a258d1ee2d90537ac3937b01c3e0f
heman0805 <1317833140@qq.com>
2020-03-25 20:58:29 +0800

    springboot与activiti6整合并禁用security

--- src/main/java/com/heman/bysj/BysjApplication.java
+++ src/main/java/com/heman/bysj/BysjApplication.java
@@ -1,13 +1,14 @@
 package com.heman.bysj;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication(
-        exclude = {
-                org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-                org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
-        }
+
+        exclude = SecurityAutoConfiguration.class
 )
 public class BysjApplication {
 
--- src/main/java/com/heman/bysj/controller/test.java
+++ src/main/java/com/heman/bysj/controller/test.java
@@ -1,34 +1,75 @@
 package com.heman.bysj.controller;
 
-import com.heman.bysj.utils.SecurityUtil;
-import org.activiti.api.process.model.ProcessInstance;
-import org.activiti.api.process.model.builders.ProcessPayloadBuilder;
-import org.activiti.api.process.runtime.ProcessRuntime;
-import org.activiti.api.task.runtime.TaskRuntime;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.activiti.engine.*;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class test {
+    @Resource
+    private RuntimeService runtimeService;
+/*
 
     @Autowired
-    private ProcessRuntime processRuntime;
+    private RuntimeService runtimeService;
     @Autowired
-    private SecurityUtil securityUtil;
+    private TaskService taskService;
+
+    @Autowired
+    private IdentityService identityService;
+
+    @Autowired
+    private RepositoryService repositoryService;
+
+    @Autowired
+    private ProcessEngine processEngine;
+
+    @Autowired
+    private HistoryService historyService;
+*/
 
     @RequestMapping(value="/test")
     public void test2(){
         System.out.println("进入test");
-        securityUtil.logInAs("salaboy");
-        ProcessInstance processInstance = processRuntime.start(ProcessPayloadBuilder
+/*
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+
+        //2.得到RunService对象
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+
+        Map<String,Object> map = new HashMap<>();
+        map.put("num",4);
+        //3.创建流程实例  流程定义的key需要知道 holiday
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("holiday",map);
+
+        System.out.println("流程部署ID"+processInstance.getDeploymentId());//null
+        System.out.println("流程定义ID"+processInstance.getProcessDefinitionId());//holiday:1:4
+        System.out.println("流程实例ID"+processInstance.getId());//2501
+        System.out.println("活动ID"+processInstance.getActivityId());//null
+*/
+
+        Map<String,Object> map = new HashMap<>();
+        map.put("num",4);
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("holiday",map);
+        System.out.println("流程部署ID"+processInstance.getDeploymentId());//null
+        System.out.println("流程定义ID"+processInstance.getProcessDefinitionId());//holiday:1:4
+        System.out.println("流程实例ID"+processInstance.getId());//2501
+        System.out.println("活动ID"+processInstance.getActivityId());//null
+
+        //        securityUtil.logInAs("salaboy");
+       /* ProcessInstance processInstance = processRuntime.start(ProcessPayloadBuilder
                 .start()
                 .withProcessDefinitionKey("holiday")
                 .withVariable("num",4)
                 .build());//启动流程实例
-
-        System.out.println(processInstance.getId());
+*/
+        // System.out.println(processInstance.getId());
 
     }
 }
